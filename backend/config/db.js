594a0888e217a9070ca21332a15cf5beac57961d
Hmn3702 <nghia03072002@gmail.com>
2025-08-04 14:31:35 +0700

    new taskController file

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,14 @@
 // config/db.js
 const mongoose = require("mongoose");
+const dotenv = require('dotenv');
+dotenv.config();
 
 // Set strictQuery explicitly to suppress the warning
 //mongoose.set('strictQuery', true);
 
 const connectDB = async () => {
   try {
+    console.log(process.env.MONGO_URI);
     await mongoose.connect(process.env.MONGO_URI);  // Remove deprecated options
     console.log("MongoDB connected successfully");
   } catch (error) {
